2ff0029fc09468c45aaf7921d8c8b5e168422e1f
f-R-e-g-g-y <88837233+f-R-e-g-g-y@users.noreply.github.com>
2022-02-18 22:39:59 +0300

    Add files via upload

---
+++ BotDiscord/Random_Words.py
@@ -0,0 +1,31 @@
+cums = ['https://tenor.com/view/cum-the-cum-here-comes-the-cum-gif-18740280'
+        , 'https://media.giphy.com/media/BVnxOYyn8A1HPgxShA/giphy.gif'
+        , 'https://media.giphy.com/media/l0HUmvVJSs1ApEhry/giphy.gif'
+        , 'https://tenor.com/view/this-is-my-kingdom-cum-cum-gif-22178722'
+        , 'https://media.giphy.com/media/l4hLQ4l8KYkB0Wnp6/giphy.gif'
+       ]
+
+kissgif = ['https://cdn.discordapp.com/attachments/799619076150001717/876165139563438121/image0.gif'
+           , 'https://cdn.discordapp.com/attachments/799619076150001717/876165389757857802/image0.gif'
+           , 'https://cdn.discordapp.com/attachments/799619076150001717/876165678380490762/image0.gif'
+           , 'https://cdn.discordapp.com/attachments/799619076150001717/876165821293011015/image0.gif'
+           , 'https://cdn.discordapp.com/attachments/799619076150001717/876166152982761492/image0.gif'
+           , 'https://media.giphy.com/media/lTQF0ODLLjhza/giphy.gif'
+           , 'https://media.giphy.com/media/dMYVHzANYb9p6/giphy.gif'
+           , 'https://media.giphy.com/media/xUOwGeHJEQ46pdsvWE/giphy.gif'
+          ]
+
+hoin = ['https://cdn.discordapp.com/attachments/799619076150001717/876210514752057344/aa26t-kcm4u.png'
+       , 'https://cdn.discordapp.com/attachments/799619076150001717/876210723691315240/aw3es-gmn8o.png'
+       , 'https://cdn.discordapp.com/attachments/799619076150001717/876211330372227112/auadq-0mz7z.png'
+       , 'https://media.discordapp.net/attachments/799619076150001717/876212767835713606/a9r33-5uoa6.png?width=755&height=566'
+       , 'https://media.discordapp.net/attachments/799619076150001717/876213804965449778/auhbk-7rd7l.png?width=755&height=566'
+      ]
+
+huggif = ['https://cdn.discordapp.com/attachments/799619076150001717/877317044259926046/image0.gif'
+          , 'https://cdn.discordapp.com/attachments/799619076150001717/877317044712902666/image1.gif'
+          , 'https://cdn.discordapp.com/attachments/799619076150001717/877317046403219566/image2.gif'
+          , 'https://cdn.discordapp.com/attachments/799619076150001717/877317373928042506/image0.gif'
+          , 'https://cdn.discordapp.com/attachments/799619076150001717/877317374389411881/image1.gif'
+          , 'https://cdn.discordapp.com/attachments/799619076150001717/877317374838185994/image2.gif'
+         ]
